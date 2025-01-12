a23159b3bb716ea503bd170a2d1d77cdac2edc06
NavCGaur <vertigo1112@gmail.com>
2025-01-12 20:50:59 +0530

    cors all origin allowed for testing

--- server/server.js
+++ server/server.js
@@ -8,20 +8,22 @@ import { seedDatabase } from './seed.js';
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+  credentials: false, // Disable credentials for unrestricted access
+}));
 app.use(express.json());
 
+// Routes
+app.use('/questions', questionsRouter);
 
-  
-  // Routes
-  app.use('/questions', questionsRouter);
+// Database connection
+mongoose.connect(config.mongoUri)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB:', err));
 
-  // Database connection
-  mongoose.connect(config.mongoUri)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Could not connect to MongoDB:', err));
-
-  // Start server
-  app.listen(config.port, () => {
-    console.log(`Server running on port ${config.port}`);
-  });
+// Start server
+app.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`);
+});
